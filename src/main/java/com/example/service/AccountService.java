1fac0d658e77a98b5f2a4fd4a31d82573ea70cbb
AF-Foysal <afoysal0422@gmail.com>
2024-09-03 06:36:05 +0000

    Stories 1 & 2 Service and Repository Initiated

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public void register(Account account){
+        if ((!account.getUsername().isEmpty()) && (account.getPassword().length() >= 4)){
+            accountRepository.save(account);
+        }
+    }
+
+    public void login(Account account) throws AuthenticationException {
+        accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
+    }
 }
