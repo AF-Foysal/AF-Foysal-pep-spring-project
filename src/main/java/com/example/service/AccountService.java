61b2ea6c5f73f92603f75ef0ab07b76bfec689b3
AF-Foysal <afoysal0422@gmail.com>
2024-09-03 18:55:46 +0000

    Basic Implementation of all Stories within the Repositories

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -16,13 +16,24 @@ public class AccountService {
         this.accountRepository = accountRepository;
     }
 
+    /**
+     * Story 1: Account Registration
+     * @param account
+     */
     public void register(Account account){
         if ((!account.getUsername().isEmpty()) && (account.getPassword().length() >= 4)){
             accountRepository.save(account);
         }
     }
 
+    /**
+     * Story 2: Account Login
+     * @param account
+     * @throws AuthenticationException Account is not found or Password incorrect
+     */
     public void login(Account account) throws AuthenticationException {
         accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
     }
+
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,7 +1,11 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.entity.Message;
 import com.example.repository.MessageRepository;
 
 public class MessageService {
@@ -12,4 +16,63 @@ public class MessageService {
     public MessageService(MessageRepository messageRepository){
         this.messageRepository = messageRepository;
     }
+
+    /**
+     * Story 3: Message Creation
+     * MessageText Length must NOT be Blank nor Greater than 255
+     * @param message
+     */
+    public void createMessage(Message message){
+        if ((!message.getMessageText().isEmpty()) || (message.getMessageText().length() <= 255)){
+            messageRepository.save(message);
+        }
+    }
+
+    /**
+     * Story 4: Retrieve All Messages
+     * @return List of all Messages
+     */
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+    
+    /**
+     * Story 5: Retrieve Message by ID
+     * @param message_id
+     * @return an Optional<Message>
+     */
+    public Optional<Message> getMessageByID(int message_id){
+        return messageRepository.findById(message_id);
+    }
+
+    /**
+     * Story 6: Delete Message By ID
+     * @param message_id
+     */
+    public void deleteMessageByID(int message_id){
+        messageRepository.deleteById(message_id);
+    }
+
+    /**
+     * Story 7: Update Message Text
+     * @param message_id
+     * @param message_text
+     */
+    public void updateMessageText(int message_id, String message_text){
+        if ((messageRepository.existsById(message_id)) && (message_text.length() > 0) && (message_text.length() <= 255)){
+            Message message = messageRepository.getById(message_id);
+            message.setMessageText(message_text);
+            messageRepository.save(message);
+        }
+    }
+
+    /**
+     * Story 8: Retrieve all Messages by Account ID
+     * @param account_id
+     * @return List of all Messages
+     */
+    public List<Message> getAllMessagesByAccountID(int account_id){
+        return messageRepository.findAllByAccountID(account_id);
+    }
+
 }
